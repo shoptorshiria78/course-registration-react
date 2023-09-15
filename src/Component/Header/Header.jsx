652fa72d6df7848483c66ea7ecc6f3690512560f
shoptorshi <shoptorshiria78@gmail.com>
2023-09-15 11:57:25 +0600

    project setup with tailwind and fake data

---
+++ src/Component/Header/Header.jsx
@@ -0,0 +1,10 @@
+
+const Header = () => {
+    return (
+        <div>
+            <h1 className="text-3xl font-bold text-center mt-10">Course Registration</h1>
+        </div>
+    );
+};
+
+export default Header;
